a7ab5f4550341f8b66dd37eb462acaa43388f2b4
jordyAym <jayma@unsa.edu.pe>
2024-12-20 23:29:22 -0500

    Integracion de angular para las interfaces graficas

--- sistema_notas/rol.py
+++
@@ -1,8 +0,0 @@
-class Rol:
-    def __init__(self, nombre, permisos):
-        self.nombre = nombre
-        self.permisos = permisos  # Lista de permisos asignados al rol
-
-    def tiene_permiso(self, permiso):
-        """Verifica si el rol tiene un permiso específico."""
-        return permiso in self.permisos
